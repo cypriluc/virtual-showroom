853d5a4c7f885f0346d1bb0768258e5c6f0edb95
Lucia Cyprianova <l.cyprianova@gmail.com>
2021-03-05 16:06:30 +0100

    use node, express server, ejs to get json path automatically

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("Hello showroom!");
+const express = require("express");
+const ejs = require("ejs");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+const fs = require("fs");
+const buildFiles = fs.readdirSync(__dirname + "/public/Build/");
+let currentJson = "";
+buildFiles.forEach(function (file) {
+  let fileType = file.split(".").pop();
+  if (fileType == "json") {
+    currentJson = file;
+  }
+});
+
+app.get("/", function (req, res) {
+  res.render("home", {
+    currentJson: currentJson,
+  });
+});
+
+app.listen(3000, function () {
+  console.log("server started on port 3000");
+});
